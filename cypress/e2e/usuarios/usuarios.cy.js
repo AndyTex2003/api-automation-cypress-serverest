8ac7ed8954135b4d8948a307cef4bb698984c92f
tex batista <andersonrexbatista@gmail.com>
2026-06-08 11:09:20 -0300

    feat: adiciona fixtures para dados de teste

--- cypress/e2e/usuarios/usuarios.cy.js
+++ cypress/e2e/usuarios/usuarios.cy.js
@@ -1,24 +1,27 @@
 import { criarUsuario } from "../../services/usuariosService.js";
 
 describe('ServeRest - Usuários', () => {
-    
+
     it('Deve criar um usuário com sucesso', () => {
 
-        const usuario = {
-            nome: "QA Test Cypress",
-            email: `qa${Date.now()}@qa.com`,
-            password: '123456',
-            administrador: 'true'
-        }
+        cy.fixture('usuario').then((dadosUsuario) => {
 
-        criarUsuario(usuario)
-            .then((response) => {
+            const usuario = {
+                ...dadosUsuario,
+                email: `qa${Date.now()}@qa.com`
+            }
 
-                expect(response.status).to.eq(201)
-                expect(response.body).to.have.property('_id')
-                expect(response.body.message)
-                    .to.eq('Cadastro realizado com sucesso')
-            });
+            criarUsuario(usuario)
+                .then((response) => {
+
+                    expect(response.status).to.eq(201)
+                    expect(response.body).to.have.property('_id')
+                    expect(response.body.message)
+                        .to.eq('Cadastro realizado com sucesso')
+                });
+
+
+        });
     });
 
 });
